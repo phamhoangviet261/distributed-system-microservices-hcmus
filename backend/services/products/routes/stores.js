4a0d355d83e0bbcadaafb5966a7c3233fee27fe6
LAP12400\LAP12400-local <hoangviet.it.hcmus@gmail.com>
2022-07-05 14:48:44 +0700

    Add invoice to store

--- backend/services/products/routes/stores.js
+++ backend/services/products/routes/stores.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-
+const axios = require('axios');
 const Store = require('../models/Store')
+const Product = require('../models/Product')
+
 const mongoose = require('mongoose')
 
 router.get('/', async (req, res, next) => {
@@ -18,7 +20,30 @@ router.get('/', async (req, res, next) => {
 router.get('/:storeId', async (req, res, next) => {
     try {
         const store = await Store.findOne({id: req.params.storeId});
-        return res.status(200).json({data: store});
+        const fakeStore = JSON.parse(JSON.stringify(store));
+        const listProducts = []
+        for(let i = 0; i < fakeStore.products.length; i++) {
+            item = fakeStore.products[i];
+            let p = await Product.findOne({id: item})
+            listProducts.push(p)
+        }
+        fakeStore.productsDetail = listProducts
+
+
+        const optionFindWard = {
+            method: 'get',
+            url: `http://localhost:5001/districts/${fakeStore.address.districtId}/${fakeStore.address.wardId}`,
+            data: {},
+        };
+
+        const axiosRespondFindWard = await axios(optionFindWard);
+
+        fakeStore.addressDetail = {
+            district: axiosRespondFindWard.data.data?.district?.name,
+            ward: axiosRespondFindWard.data.data?.ward?.name,
+            detail: fakeStore.address.detail
+        };
+        return res.status(200).json({data: fakeStore});
     } catch (errors) {
         console.log(errors);
         return res.status(400).json({success: false, message: errors.message});
@@ -33,6 +58,21 @@ router.post('/add', async (req, res, next) => {
         const s = new Store({id: `STORE${stores.length}`, name, description, ownerId, address, products, invoices, status : status ? status : 'active'});
         const store = await s.save();
         return res.status(200).json({data: store});
+    } catch (errors) {
+        console.log(errors);
+        return res.status(400).json({success: false, message: errors.message});
+    }
+    
+})
+
+router.post('/addInvoice', async (req, res, next) => {
+    try {
+        const {storeId, invoice} = req.body;
+        const store = await Store.find({storeId: storeId})
+        const newInvoices = store.invoices || []
+        newInvoices.push(invoice)
+        const s = await Store.findOneAndUpdate({storeId: storeId}, {invoices: newInvoices});
+        return res.status(200).json({data: s});
     } catch (errors) {
         console.log(errors);
         return res.status(400).json({success: false, message: errors.message});
