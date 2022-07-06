ac545276e1f70174b7abcb8f309205d9e13cd4d3
LAP12400\LAP12400-local <hoangviet.it.hcmus@gmail.com>
2022-07-06 09:20:12 +0700

    Fixing mocks data product.

--- backend/services/accounts/routes/accounts.js
+++ backend/services/accounts/routes/accounts.js
@@ -3,7 +3,7 @@ const router = express.Router()
  // DO NOT USE IT
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken')
-
+const axios = require('axios');
 const Account = require('../models/Account')
 
 
@@ -21,7 +21,33 @@ router.get('/', async (req, res, next) => {
 router.get('/:phoneNumber', async (req, res, next) => {
     try {
         const account = await Account.findOne({phoneNumber: req.params.phoneNumber});
-        return res.status(200).json({data: account});
+        let fakeAccount = JSON.parse(JSON.stringify(account));
+        let invDetail = []
+        for(let i = 0; i < fakeAccount.invoices.length; i++) {
+            let inv = await axios({
+                method: 'get',
+                url: `http://localhost:5002/invoices/${fakeAccount.invoices[i]}`,
+                data: {},
+            })
+            invDetail.push(inv.data.data)
+            
+        }
+
+        const optionFindWard = {
+            method: 'get',
+            url: `http://localhost:5001/districts/${fakeAccount.address.districtId}/${fakeAccount.address.wardId}`,
+            data: {},
+        };
+
+        const axiosRespondFindWard = await axios(optionFindWard);
+
+        fakeAccount.addressDetail = {
+            district: axiosRespondFindWard.data.data?.district?.name,
+            ward: axiosRespondFindWard.data.data?.ward?.name,
+            detail: fakeAccount.address.detail
+        };
+        fakeAccount.invoicesDetail = invDetail;
+        return res.status(200).json({data: fakeAccount});
     } catch (errors) {
         console.log(errors);
         return res.status(400).json({success: false, message: errors.message});
