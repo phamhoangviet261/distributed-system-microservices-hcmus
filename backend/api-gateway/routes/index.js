ec78dbc714352f792d321aa122944c1aa6fd9554
DangPham112000 <dangpham112000@gmail.com>
2022-07-07 18:06:23 +0700

    dp update registry structure and add loadbalancer feature

--- backend/api-gateway/routes/index.js
+++ backend/api-gateway/routes/index.js
@@ -2,61 +2,67 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const registry = require('./registry.json');
+const loadbalancer = require('../utils/loadbalancer');
 const fs = require('fs');
 
-router.post('/register', async (req, res, next) => {
-    try {
-        const resgistration = req.body;
+// router.post('/register', async (req, res, next) => {
+//     try {
+//         const resgistration = req.body;
 
-        resgistration.url = resgistration.protocol + '://' + resgistration.host + ':' + resgistration.port + '/';
-        registry.services[resgistration.apiName] = {...resgistration};
+//         resgistration.url = resgistration.protocol + '://' + resgistration.host + ':' + resgistration.port + '/';
+//         registry.services[resgistration.apiName] = {...resgistration};
 
-        fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
-            if (error) {
-                throw new Error(`Could not register [${resgistration.apiName}] \n${error.message}`);
-            } else {
-                res.send(`Successfully registered [${resgistration.apiName}]`);
-            }
-        });
-    } catch (error) {
-        console.log('Error: ', error.message);
-        return res.status(400).json({success: false, message: error.message});
-    }
-});
+//         fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
+//             if (error) {
+//                 throw new Error(`Could not register [${resgistration.apiName}] \n${error.message}`);
+//             } else {
+//                 res.send(`Successfully registered [${resgistration.apiName}]`);
+//             }
+//         });
+//     } catch (error) {
+//         console.log('Error: ', error.message);
+//         return res.status(400).json({success: false, message: error.message});
+//     }
+// });
 
-router.post('/unregister', async (req, res, next) => {
-    try {
-        const {apiName} = req.body;
-        if (!registry.services[apiName]) {
-            throw new Error(`Service name [${apiName}] does not exist`);
-        }
+// router.post('/unregister', async (req, res, next) => {
+//     try {
+//         const {apiName} = req.body;
+//         if (!registry.services[apiName]) {
+//             throw new Error(`Service name [${apiName}] does not exist`);
+//         }
 
-        delete registry.services[apiName];
+//         delete registry.services[apiName];
 
-        fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
-            if (error) {
-                throw new Error(`Could not unregister [${apiName}] \n${error.message}`);
-            } else {
-                res.send(`Successfully unregistered [${apiName}]`);
-            }
-        });
-    } catch (error) {
-        console.log('Error: ', error.message);
-        return res.status(400).json({success: false, message: error.message});
-    }
-});
+//         fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
+//             if (error) {
+//                 throw new Error(`Could not unregister [${apiName}] \n${error.message}`);
+//             } else {
+//                 res.send(`Successfully unregistered [${apiName}]`);
+//             }
+//         });
+//     } catch (error) {
+//         console.log('Error: ', error.message);
+//         return res.status(400).json({success: false, message: error.message});
+//     }
+// });
 
 router.use('/:apiName', async (req, res, next) => {
     try {
+        console.log('\n', new Date());
         const {apiName} = req.params;
         const {path} = req;
-        if (!registry.services[apiName]) {
+        const service = registry.services[apiName];
+        if (!service) {
             throw new Error(`Service name [${apiName}] does not exist`);
         }
         console.log(`Service [${apiName}] has been called`);
+        const newIndex = loadbalancer(service);
+        const serviceInstance = service.instances[newIndex];
+        console.log(`Instance [${newIndex}] is using`);
         const options = {
             method: req.method,
-            url: registry.services[apiName].url + path,
+            url: serviceInstance.url + path,
             data: req.body,
             headers: {'content-type': req.headers['content-type']}
         };
