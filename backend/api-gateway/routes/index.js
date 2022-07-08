f05b51492e9a9b331a5a9693dd50533274370f19
DangPham112000 <dangpham112000@gmail.com>
2022-07-08 17:48:33 +0700

    fix register

--- backend/api-gateway/routes/index.js
+++ backend/api-gateway/routes/index.js
@@ -5,47 +5,64 @@ const registry = require('./registry.json');
 const loadbalancer = require('../utils/loadbalancer');
 const fs = require('fs');
 
-// router.post('/register', async (req, res, next) => {
-//     try {
-//         const resgistration = req.body;
+router.post('/register', async (req, res, next) => {
+    try {
+        const resgistration = req.body;
+        resgistration.url = resgistration.protocol + '://' + resgistration.host + ':' + resgistration.port;
 
-//         resgistration.url = resgistration.protocol + '://' + resgistration.host + ':' + resgistration.port + '/';
-//         registry.services[resgistration.apiName] = {...resgistration};
+        if (registry.services[resgistration.apiName]) {
+            const checkExist = registry.services[resgistration.apiName].instances.some((instance) => {
+                let isSame = true;
+                for (let field in instance) {
+                    if (instance[field] !== resgistration[field]) {
+                        isSame = false;
+                        break;
+                    }
+                }
+                if (isSame) return true;
+            });
+            if (checkExist) {
+                throw new Error(`This instance already exists in [${resgistration.apiName}]`);
+            }
+            registry.services[resgistration.apiName].instances.push({...resgistration});
+        } else {
+            registry.services[resgistration.apiName] = {index: 0, instances: [{...resgistration}]};
+        }
 
-//         fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
-//             if (error) {
-//                 throw new Error(`Could not register [${resgistration.apiName}] \n${error.message}`);
-//             } else {
-//                 res.send(`Successfully registered [${resgistration.apiName}]`);
-//             }
-//         });
-//     } catch (error) {
-//         console.log('Error: ', error.message);
-//         return res.status(400).json({success: false, message: error.message});
-//     }
-// });
+        fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
+            if (error) {
+                throw new Error(`Could not register [${resgistration.apiName}] \n${error.message}`);
+            } else {
+                res.send(`Successfully registered [${resgistration.apiName}]`);
+            }
+        });
+    } catch (error) {
+        console.log('Error: ', error.message);
+        return res.status(400).json({success: false, message: error.message});
+    }
+});
 
-// router.post('/unregister', async (req, res, next) => {
-//     try {
-//         const {apiName} = req.body;
-//         if (!registry.services[apiName]) {
-//             throw new Error(`Service name [${apiName}] does not exist`);
-//         }
+router.post('/unregister', async (req, res, next) => {
+    try {
+        const {apiName} = req.body;
+        if (!registry.services[apiName]) {
+            throw new Error(`Service name [${apiName}] does not exist`);
+        }
 
-//         delete registry.services[apiName];
+        delete registry.services[apiName];
 
-//         fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
-//             if (error) {
-//                 throw new Error(`Could not unregister [${apiName}] \n${error.message}`);
-//             } else {
-//                 res.send(`Successfully unregistered [${apiName}]`);
-//             }
-//         });
-//     } catch (error) {
-//         console.log('Error: ', error.message);
-//         return res.status(400).json({success: false, message: error.message});
-//     }
-// });
+        fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
+            if (error) {
+                throw new Error(`Could not unregister [${apiName}] \n${error.message}`);
+            } else {
+                res.send(`Successfully unregistered [${apiName}]`);
+            }
+        });
+    } catch (error) {
+        console.log('Error: ', error.message);
+        return res.status(400).json({success: false, message: error.message});
+    }
+});
 
 router.use('/:apiName', async (req, res, next) => {
     try {
